9a813e265c5794ed76b5da63334677bfb948985d
Rahul Gupta <rahul.gupta@agrexai.com>
2024-10-17 12:54:46 +0530

    Initial commit

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,23 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth";
-import {getFirestore} from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDrB9FupDgjjVk4_guLC24ydG3retE3bME",
-  authDomain: "login-auth-4736e.firebaseapp.com",
-  projectId: "login-auth-4736e",
-  storageBucket: "login-auth-4736e.appspot.com",
-  messagingSenderId: "10562914305",
-  appId: "1:10562914305:web:2cff37be4fa9ccf0a29800"
+  apiKey: "AIzaSyBeD33GSXkmnMjimbvLglie07oToexSSWk",
+  authDomain: "netflixgpt-bc096.firebaseapp.com",
+  projectId: "netflixgpt-bc096",
+  storageBucket: "netflixgpt-bc096.appspot.com",
+  messagingSenderId: "586043445942",
+  appId: "1:586043445942:web:81dfcc06f7f387481bcc17",
+  measurementId: "G-SZSS9PH2GT",
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const auth=getAuth();
-export const db=getFirestore(app);
+export const auth = getAuth();
+export const db = getFirestore(app);
 export default app;
